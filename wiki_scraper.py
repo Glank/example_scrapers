a85528cf56318dba3277bb81b32e16f1757a8c93
Ernest Kirstein <glank314@gmail.com>
2014-06-18 21:15:13 -0400

    Finished Class Lab

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,5 +1,6 @@
 #Author: Ernest Kirstein
 
+import re
 from simple_cache import Cache
 from BeautifulSoup import BeautifulSoup
 import urllib2   
@@ -23,21 +24,36 @@ class Country:
         self.name = None
         self.population = None
     def is_valid(self):
-        if not re.match("[\w ]+$", self.name):
+        if not re.match(r"[\(\)\w \-,]+$", self.name):
             return False
         if not isinstance(self.population, int):
             return False
         return True
+    def __repr__(self):
+        return self.name+": "+str(self.population)
 
 def get_country(row):
     print row
     exit
 
 def get_countries(source):
-    soup = BeautifulSoup(source)
-    print soup.findAll("table", {"wikitable sortable jquery-tablesorter"})
+    soup = BeautifulSoup(source.decode("utf-8").encode("ascii", "ignore"))
+    table = soup.find("table", {'class':'wikitable sortable'})
+    rows = table.findAll("tr")
+    countries = []
+    for row in rows[1:]:
+        cells = row.findAll("td")
+        country = Country()
+        country.name = str(cells[1].find('a').string)
+        country.population = int("".join(re.findall('\d',str(cells[2].string))))
+        if not country.is_valid():
+            raise Exception(str(country))
+        countries.append(country)
+    return countries
 
 if __name__=="__main__":
     url = "http://en.wikipedia.org/wiki/List_of_countries_by_population"
     source = get_cached_page_html(url)
-    print source
+    countries = get_countries(source)
+    for country in countries:
+        print country
